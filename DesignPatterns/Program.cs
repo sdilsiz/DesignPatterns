d5caf19db7a717c40c2e839e1a83716788f01b6b
sdilsiz <dilsizsalih@gmail.com>
2020-10-24 20:37:52 +0200

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.WebSockets;
+ 
+namespace DesignPatterns {
+    class Program {
+        static void Main(string[] args) {
+
+
+            
+            //AbstractFactory.Structural.MainApp.Main();
+            //AbstractFactory.RealWorld.MainApp.Main();
+
+
+         
+            Builder.Structural.MainApp.Main();
+            Builder.RealWorld.MainApp.Main();
+
+            Console.ReadKey();
+        }
+    }
+}
